61b1d657fb8cb6545a09f7a1bc239b4717be6c8b
Siddu Harake <harakesiddu5@gmail.com>
2026-05-18 09:12:23 -0700

    Add files via upload

---
+++ Java script/1st.js
@@ -0,0 +1,6 @@
+let price=100;      numerical type
+let name ="tony";   string type
+isfollow=true;      bolean type
+let isfollow=true;  un-defined type
+let x=null;         nulltype
+let x= BigInt(123)
