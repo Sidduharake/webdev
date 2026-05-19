30c40fe064644ac21b26b551078a9e642621f885
Siddu Harake <harakesiddu5@gmail.com>
2026-05-19 10:05:14 -0700

    Add files via upload

---
+++ Java script/2st.js
@@ -0,0 +1,21 @@
+//FUNCTIONS AND DATA TYPES
+//practice question 1
+const product={
+    name:"parker jotter  standard ball pen",
+    price:270,
+    rating:4,
+};
+console.log( product );
+
+
+//practice question 2
+const profile= {
+    username:"viratkohli",
+    followers:700,
+    following:20,
+    isFollow:true,
+};
+console.log( profile );
+ 
+
+
