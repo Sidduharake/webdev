d41f9d1219bbbe788708208b8027a03225f7c7a6
Siddu Harake <harakesiddu5@gmail.com>
2026-05-20 09:08:54 -0700

    Add files via upload

---
+++ Java script/3st.js
@@ -0,0 +1,34 @@
+//arithmatic operators 
+/*let a=5;
+let b=3;
+
+console.log("a + b =",a + b)
+console.log("a - b =",a - b)
+console.log("a * b =",a * b)
+console.log("a / b =",a / b)
+console.log("a % b =",a % b)
+console.log("a ** b =",a ** b)
+
+//asssignment operators (+=,-=,*=,%=,**=)
+let a=5;
+let b=9;
+
+b+=2;
+console.log("b=",b);
+
+//comparison operator(==,!=,=== strick check with data type,!==,<=,>=)
+let a=5;
+let b="5";//string number
+
+console.log("a===b",a===b)*/
+
+//logical operator(&&,||,!)
+let a=5;
+let b=9;
+
+let cond1= a<9;
+let cond2= a==5;
+
+console.log("cond1&&cond2=",cond1&&cond2)
+
+
